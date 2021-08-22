6536ee4fd69d3a818aee0dbf015129192adab78a
MSI\Tanay <tanaykulkarni7@gmail.com>
2021-08-22 16:51:55 +0530

    added .env to gitignore

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,52 +1,48 @@
 import react from "react";
 
-class Signup extends react.Component{
-
-    constructor(){
+class Signup extends react.Component {
+    constructor() {
         super()
         this.state = {
-            name:"",
-            email:"",
-            password:"",
+            name: "",
+            email: "",
+            password: "",
         };
-        
     }
 
-
-    inputName = (event) =>{
+    inputName = (event) => {
         this.setState({
             name: event.target.value
         })
     }
 
-    inputEmail = (event) =>{
+    inputEmail = (event) => {
         this.setState({
             email: event.target.value
         })
     }
 
-    inputPassword = (event) =>{
+    inputPassword = (event) => {
         this.setState({
             password: event.target.value
         })
     }
 
-    onSubmit = (event) =>{
+    onSubmit = (event) => {
         event.preventDefault()
         console.log(this.state.name);
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="container tc">
                 <h1>Signup</h1>
-                <form onSubmit = {this.onSubmit}>
-                <label>Name     :</label><input type="text" value={this.state.name} onChange = {this.inputName} placeholder="Enter Your Name"></input><br/>
-                <label>Email    :</label><input type="text" value={this.state.email} onChange = {this.inputEmail} placeholder="Enter Your Email"></input><br/>
-                <label>Password :</label><input type="text" value={this.state.password} onChange = {this.inputPassword}placeholder="Enter Your Password"></input><br/>
-                <input type="submit" value="submit"></input>
+                <form onSubmit={this.onSubmit}>
+                    <label>Name     :</label><input type="text" value={this.state.name} onChange={this.inputName} placeholder="Enter Your Name"></input><br />
+                    <label>Email    :</label><input type="text" value={this.state.email} onChange={this.inputEmail} placeholder="Enter Your Email"></input><br />
+                    <label>Password :</label><input type="text" value={this.state.password} onChange={this.inputPassword} placeholder="Enter Your Password"></input><br />
+                    <input type="submit" value="submit"></input>
                 </form>
-                
             </div>
         )
     }
