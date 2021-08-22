4b64364e0ddb5531b10b60823fe7aa5d38f6b76b
panchalpavan <luckiepanchal@gmail.com>
2021-08-22 22:13:50 +0530

    small changes

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,4 +1,5 @@
 import react from "react";
+import "./Signup.css"
 
 class Signup extends react.Component{
 
@@ -33,18 +34,21 @@ class Signup extends react.Component{
 
     onSubmit = (event) =>{
         event.preventDefault()
-        console.log(this.state.name);
+        const arr = [];
+        arr.push(this.state);
+        console.log(arr);
+        console.log(this.state);
     }
 
     render(){
         return(
-            <div className="container tc">
+            <div className="container">
                 <h1>Signup</h1>
-                <form onSubmit = {this.onSubmit}>
-                <label>Name     :</label><input type="text" value={this.state.name} onChange = {this.inputName} placeholder="Enter Your Name"></input><br/>
-                <label>Email    :</label><input type="text" value={this.state.email} onChange = {this.inputEmail} placeholder="Enter Your Email"></input><br/>
-                <label>Password :</label><input type="text" value={this.state.password} onChange = {this.inputPassword}placeholder="Enter Your Password"></input><br/>
-                <input type="submit" value="submit"></input>
+                <form onSubmit = {this.onSubmit} className="formContainer"> 
+                <label>Name:</label><input className = "tc pa2 ma1 br3" type="text" value={this.state.name} onChange = {this.inputName} placeholder="Enter Your Name"></input><br/>
+                <label>Email:</label><input className = "tc pa2 ma1 br3" type="text" value={this.state.email} onChange = {this.inputEmail} placeholder="Enter Your Email"></input><br/>
+                <label>Password:</label><input className = "tc pa2 ma1 br3" type="text" value={this.state.password} onChange = {this.inputPassword}placeholder="Enter Your Password"></input><br/>
+                <input className = "tc pa2 ma1 br3 button" type="submit" value="submit"></input>
                 </form>
                 
             </div>
