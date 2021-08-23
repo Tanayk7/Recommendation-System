db0b080fff85debf19b539184488e7d8514b535a
MSI\Tanay <tanaykulkarni7@gmail.com>
2021-08-23 18:01:06 +0530

    added eventEmitter and fsutils

---
+++ server/utils/eventEmitter.js
@@ -0,0 +1,33 @@
+const events = {}
+
+class EventEmitter {
+    constructor() {
+        this.events = [];
+    }
+
+    subscribe(eventName, fn) {
+        if (!this.events[eventName]) {
+            this.events[eventName] = [];
+        }
+        this.events[eventName].push(fn);
+
+        return () => {
+            this.events[eventName] = this.events[eventName].filter(
+                (eventFn) => fn !== eventFn
+            );
+        };
+    }
+
+    emit(eventName, data) {
+        const event = this.events[eventName];
+        if (event) {
+            event.forEach((fn) => {
+                fn.call(null, data);
+            });
+        }
+    }
+}
+
+const eventEmitter = new EventEmitter();
+
+module.exports = { eventEmitter, events };
---
+++ server/utils/fs-util.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    getCurrentDirectoryBase: () => {
+        return path.basename(process.cwd());
+    },
+
+    directoryExists: (filePath) => {
+        return fs.existsSync(filePath);
+    },
+
+    list_files: (dir_path) => {
+        try {
+            let file_names = fs.readdirSync(dir_path);
+            return file_names;
+        }
+        catch (err) {
+            console.log(err);
+            return err;
+        }
+    },
+
+    create_directory: (dir_path) => {
+        try {
+            fs.mkdirSync(dir_path);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    delete_directory: (dir_path) => {
+        try {
+            fs.rmdirSync(dir_path, { recursive: true });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    readFile: (file_path) => {
+        return fs.readFileSync(file_path, { encoding: 'utf8' });
+    },
+
+    writeFile: (file_path, content = "") => {
+        try {
+            fs.writeFileSync(file_path, content);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    deleteFile: (path) => {
+        try {
+            fs.unlinkSync(path)
+        } catch (err) {
+            console.error(err)
+        }
+    },
+};
